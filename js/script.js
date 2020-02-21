af955fdeef0e501e290e46f85ff3557fa49e64cc
EugeneAndrievsky <andrievsky.eu@yandex.ru>
2020-02-21 20:58:50 +0300

    меню на js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿var menu = document.querySelector('.main-navigation');
+var menuButton = menu.querySelector('.menu-button');
+
+// открываем меню, нажимая на закрытое меню
+menu.addEventListener('click', function (e) {
+  menu.classList.remove('main-navigation--closed');
+  menu.classList.add('main-navigation--opened');
+});
+
+// открываем/закрываем меню, нажимая на кнопку
+menuButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  menu.classList.toggle('main-navigation--opened');
+  menu.classList.toggle('main-navigation--closed');
+});
+
+
